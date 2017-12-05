b0cac75443c217f8fd794d31547f724fdc795359
Rongxin Sun <sunrongxin7666@163.com>
2017-12-05 21:45:30 +0800

    Initial commit

---
+++ src/main/java/srx/awesome/code/hdyl/controller/content/DemoController.java
@@ -0,0 +1,19 @@
+package srx.awesome.code.hdyl.controller.content;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@RequestMapping("/initList")
+	public String initList() {
+		return "/content/list";
+	}
+	
+	@RequestMapping("/initModify")
+	public String initModify() { 
+		return "/content/modify";
+	}
+}
---
+++ src/main/java/srx/awesome/code/hdyl/dto/AdDto.java
@@ -0,0 +1,30 @@
+package srx.awesome.code.hdyl.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class AdDto {
+	private String title;
+    private String img;
+    private String link;
+    
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+}
